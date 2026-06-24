d0f4b94b6ff2d72db4dd24db33b2b891ab9768b7
Kenechukwu <Kenechukwukennedy588@gmail.com>
2026-06-24 16:13:42 +0100

    Make expense tracker responsive

--- src/modules/OverviewComponent.jsx
+++ src/modules/OverviewComponent.jsx
@@ -130,7 +130,7 @@ const ExpenseBox = styled.div`
 display: flex;
 flex-direction: column;
 flex: 1;
-padding: 15px 10px;
+padding: 15px 6px;
 border-radius: 4px;
 border: 1px solid #e6e8e9;
 width: 100%;
